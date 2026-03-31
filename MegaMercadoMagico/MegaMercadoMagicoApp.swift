e6f0479943493c9cdee5ef862183e7cb1c28e5b7
kleber <klever>
2026-03-30 23:07:10 -0500

    Elemino el modelo por defecto y la vista por defecto

--- MegaMercadoMagico/MegaMercadoMagicoApp.swift
+++ MegaMercadoMagico/MegaMercadoMagicoApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct MegaMercadoMagicoApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
@@ -25,7 +25,7 @@ struct MegaMercadoMagicoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           
         }
         .modelContainer(sharedModelContainer)
     }
---
+++ MegaMercadoMagico/ModeloBaseDatos.swift
@@ -0,0 +1,8 @@
+//
+//  ModeloBaseDatos.swift
+//  MegaMercadoMagico
+//
+//  Created by kleber oswaldo muy landi on 30/3/26.
+//
+
+import Foundation
