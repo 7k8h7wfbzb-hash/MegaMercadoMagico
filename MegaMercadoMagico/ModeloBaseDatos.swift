f8842e0b496564f87ef57b0f625d47836a998d7a
kleber <klever>
2026-03-30 23:08:24 -0500

    creacion del modelo cliente

--- MegaMercadoMagico/ModeloBaseDatos.swift
+++ MegaMercadoMagico/ModeloBaseDatos.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class Cliente{
+    private var id: UUID
+    private var cedula: String
+    private var nombre: String
+    private var apellido: String
+    private var edad: Int
+    private var genero: String
+    private var fechaNacimiento: Date
+    private var email: String
+    private var telefono: String
+    private var direccion: String
+    private var imagen: Data?
+    
+    init(id: UUID = UUID(), cedula: String, nombre: String, apellido: String, edad: Int, genero: String, fechaNacimiento: Date, email: String, telefono: String, direccion: String, imagen: Data? = nil) {
+        self.id = id
+        self.cedula = cedula
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        self.genero = genero
+        self.fechaNacimiento = fechaNacimiento
+        self.email = email
+        self.telefono = telefono
+        self.direccion = direccion
+        self.imagen = imagen
+    }
+}
